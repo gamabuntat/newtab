7d8620d3d2847716ed878c54bd9d9a2220cb09d7
gamabunta <gaambunta@tuta.io>
2020-05-18 04:32:27 +0800

    days

--- stopwatch.js
+++ stopwatch.js
@@ -1,15 +1,32 @@
 function watch(a) {
 
   let now = new Date();
-  let start = new Date(2020, 4, 17, 13)
-  let diff = new Date(now - start);
-  let hours = diff.getHours();
+  let start = new Date(2020, 4, 17, 13, 11, 12)
+  let diff = (now - start);
+  let seconds = Math.floor(diff / 1000);
+  let minutes = 0;
+  let hours = 0;
+  let days = 0;
+
+  if (seconds >= 60) {
+    minutes = Math.floor(seconds / 60);
+    seconds = seconds - minutes * 60;
+  }
+
+  if (minutes >= 60) {
+    hours = Math.floor(minutes / 60);
+    minutes = minutes - hours * 60;
+  }
+
+  if (hours >= 24) {
+    days = Math.floor(hours / 24);
+    hours = hours - days * 24;
+  }
+
   if (hours < 10) hours = "0" + hours;
-  let minutes = diff.getMinutes();
   if (minutes < 10) minutes = "0" + minutes;
-  let seconds = diff.getSeconds();
   if (seconds < 10) seconds = "0" + seconds;
-  let clock = `${hours} : ${minutes} : ${seconds}`
+  let clock = `Days = ${days} | ${hours} : ${minutes} : ${seconds}`
 
   if (clock != document.getElementById('idClock').innerHTML) {
     document.getElementById('idClock').innerHTML = clock;
