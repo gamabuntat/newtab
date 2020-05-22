e5c787f97f48ad9d3042e76bf42f4edf819707f8
gamabuntat <55852712+gamabuntat@users.noreply.github.com>
2020-05-22 08:13:05 +0800

    clock

---
+++ clock.js
@@ -0,0 +1,22 @@
+
+let timerId;
+
+window.onload = function () {
+	function setClock() {
+		let date = new Date();
+		let seconds = date.getSeconds();
+		let minutes = date.getMinutes();
+		let hours = date.getHours();
+		if (seconds < 10) seconds = "0" + seconds;
+		if (minutes < 10) minutes = "0" + minutes;
+		if (hours < 10) hours = "0" + hours;
+		let string = `${hours} : ${minutes} : ${seconds}`;
+	  document.getElementById('date').innerHTML = string;
+	}
+	timerId = setInterval(setClock, 1000)
+}
+
+
+function stop() {
+	clearTimeout(timerId);
+}
